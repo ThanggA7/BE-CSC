c708a039a30f0cf74c813b7db47cb1dc775082f1
Thangga7 <beencules@gmail.com>
2024-11-06 22:23:12 +0700

    hjhj

--- src/models/Report.js
+++ src/models/Report.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const reportSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  uel: { type: String, required: true },
+  url: { type: String, required: true },
   category: { type: String, required: true },
   description: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  isApproved: { type: Boolean, default: false }, 
+  isApproved: { type: Boolean, default: false },
 });
 
 // Tạo model Report
