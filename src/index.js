4773eb6cf6b033cebe5ee7c4c978e1f21c24120b
Nhu Thang Luu <59923452+ThanggA7@users.noreply.github.com>
2024-12-21 22:25:38 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,25 +4,21 @@ const cors = require("cors");
 require("dotenv").config();
 
 const reportRoutes = require("./routes/reportRoutes");
-
+const userRoutes = require("./routes/userRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Kết nối đến MongoDB
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log("Error connecting to MongoDB:", error));
 
-// Cấu hình CORS cho phép tất cả các origin
-app.use(cors()); // Cho phép tất cả các origin
+app.use(cors());
 
 app.use(express.json());
 
-// Sử dụng các route cho báo cáo
 app.use("/api", reportRoutes);
-
-// Khởi động server
+app.use("/api", userRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
