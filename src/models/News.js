76aa2edcce782ab7d4487fcf725d1d670237260a
Nhu Thang Luu <beencules@gmail.com>
2024-12-25 00:50:52 +0700

    Update News.js

--- src/models/News.js
+++ src/models/News.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const NewsSchema = new mongoose.Schema({
   title: { type: String, required: true },
   thumbnail: { type: String, required: true },
+  description: { type: String, required: true },
   content: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
