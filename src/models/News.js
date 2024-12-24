519a6c404230241f87cafd62efb7727ef654afca
Nhu Thang Luu <beencules@gmail.com>
2024-12-24 23:27:16 +0700

    Add files via upload

---
+++ src/models/News.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const NewsSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  thumbnail: { type: String, required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const News = mongoose.model("News", NewsSchema);
+
+module.exports = News;
