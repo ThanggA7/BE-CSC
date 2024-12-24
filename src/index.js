3b24757bda2d3ee68d37a45877cb220b4f93685b
Nhu Thang Luu <beencules@gmail.com>
2024-12-24 23:28:37 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 
 const reportRoutes = require("./routes/reportRoutes");
 const userRoutes = require("./routes/userRoutes");
+const newRoute = require("./routes/newsRoute");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -33,6 +34,7 @@ app.use(express.json());
 
 app.use("/api", reportRoutes);
 app.use("/api", userRoutes);
+app.use("/api", newRoute);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
