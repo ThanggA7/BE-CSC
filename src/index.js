29dfa7fda2a97b15ba81a66e1a5c33571f26d742
Nhu Thang Luu <thangln.22ns@vku.udn.vn>
2025-01-03 22:39:55 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 const reportRoutes = require("./routes/reportRoutes");
 const userRoutes = require("./routes/userRoutes");
 const newRoute = require("./routes/newsRoute");
+const notiRoutes = require("./routes/notiRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -15,19 +16,6 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log("Error connecting to MongoDB:", error));
 
-app.get("/api-chong-lua-dao", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://chongluadao.vn/datatables-server-side-processing?type=blacklist&draw=1&columns%5B0%5D%5Bdata%5D=0&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=1&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=2&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=false&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=3&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=4&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=desc&start=0&length=5000&search%5Bvalue%5D=&search%5Bregex%5D=false&_=1732542948227",
-      {
-        params: req.query,
-      }
-    );
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).send("Error fetching data");
-  }
-});
 app.use(cors());
 
 app.use(express.json());
@@ -35,9 +23,9 @@ app.use(express.json());
 app.use("/api", reportRoutes);
 app.use("/api", userRoutes);
 app.use("/api", newRoute);
-
+app.use("/api/noti", notiRoutes);
 app.get('/', (req, res) => {
-    res.redirect('http://172.0.0.1:1337/flag.txt');
+    res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 });
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
