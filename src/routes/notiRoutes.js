6645a8b446481854ddfe46903d70faf32b37da4f
Nhu Thang Luu <thangln.22ns@vku.udn.vn>
2025-01-03 23:31:29 +0700

    Update notiRoutes.js

--- src/routes/notiRoutes.js
+++ src/routes/notiRoutes.js
@@ -1,11 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const notiController = require("../controllers/notiController");
+const notiController = require('../controllers/notiController');
 
-router.post("/create", notiController.createNoti);
+router.post('/create', notiController.createNoti);
 
-router.get("/:slug", notiController.getNoti);
+router.get('/:slug', notiController.getNoti);
 
-router.delete("/:slug", notiController.deleteNoti);
+router.delete('/:slug', notiController.deleteNoti);
+
+router.put('/:slug', notiController.updateNoti);
 
 module.exports = router;
